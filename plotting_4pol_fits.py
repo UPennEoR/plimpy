e9d8ab3122a5cda2bde9acb366c3800626d8647d
Saul A. Kohn <saul.aryeh.kohn@gmail.com>
2017-12-04 16:57:19 -0500

    updating plotting routine

--- plotting_4pol_fits.py
+++ plotting_4pol_fits.py
@@ -1,29 +1,71 @@
 from astropy.io import fits
 import numpy as np, matplotlib.pyplot as plt
 import sys
-from astropy import wcs
+from matplotlib import gridspec
 
-hdulist = fits.open(sys.argv[0])
-w = wcs.WCS(hdulist[0].header)
+norm = True
 
-# here: read in CRVAL and CRPIX - 1 (fortran indexing) and CDELT to get ra_{min,max} and dec_{min,max}
+hdulist = fits.open('4pol2457548.45923.fits')
+hdr = hdulist[0].header
+
+racen,deccen = hdr['CRVAL1'],hdr['CRVAL2']
+racenpix,deccenpix = int(hdr['CRPIX1'])-1, int(hdr['CRPIX2'])-1
+radelt,decdelt = hdr['CDELT1'],hdr['CDELT2']
+
+ramin = racen + (radelt*racenpix)
+ramax = racen - (radelt*racenpix)
+decmin = deccen - (decdelt*deccenpix)
+decmax = deccen + (decdelt*deccenpix)
+extent = (ramin,ramax,decmin,decmax)
+
+nrow,ncol = 2,2
+
+top=1.-0.5/(nrow+1)
+bottom=0.5/(nrow+1)
+left=0.5/(ncol+1)
+right=1-0.5/(ncol+1)
 
 f,axarr = plt.subplots(2,2,sharex=True,sharey=True)
-for i in range
-    if i == 0:
-        vmax = 0.6
-        vmin = 0.
-        cmap = 'viridis'
+gs = gridspec.GridSpec(2, 2, width_ratios=[1, 1], wspace=0.01, hspace=0.1, top=top,
+                       bottom=bottom, left=left, right=right) 
+iarr = [0,0,1,1]
+jarr = [0,1,0,1]
+for i in range(4):
+    ax = plt.subplot(gs[iarr[i],jarr[i]])
+    if not norm:
+        if i == 0:
+            vmax = 0.6
+            vmin = -0.025
+            cmap = 'viridis'
+        else:
+            vmax = 0.008
+            vmin = -0.008
+            cmap = 'RdYlGn'
     else:
-        vmax = 0.008
-        vmin = -0.008
-        cmap = 'RdYlGn'
-    ax = axarr.ravel()[i]
-    # extent = (ramin,ramax,decmin,decmax)
-    im = ax.imshow(np.arcsinh(test.data[i,0,:,:])[::-1],cmap=cmap,vmax=vmax,vmin=vmin) #,extent=extent)
+        if i==0:
+            vmax = 1.
+            vmin = -0.04
+            cmap = 'viridis'
+        else:
+            vmax = 0.01
+            vmin = -0.01
+            cmap = 'RdYlGn'
+    if i<=1:
+        ax.set_xticklabels([])
+    else:
+        ax.set_xlabel('R.A. [deg.]',size=12)
+        
+    if i in [1,3]:
+        #ax.set_yticklabels([])
+        print('yay')
+    else:
+        ax.set_ylabel('Dec. [deg.]',size=12)
+    if norm:
+        N = 0.6
+    else:
+        N = 1.
+    img = hdulist[0].data[i,0,:,:][::-1]/N #np.arcsinh(hdulist[0].data[i,0,:,:])[::-1]
+    im = ax.imshow(img,cmap=cmap,vmax=vmax,vmin=vmin,extent=extent)
     f.colorbar(im,ax=ax)
-# somehow do axis labels (dones't  reallly matter which way round)
-# TODO: get rid of white space around images??
-f.tight_layout()
+#plt.savefig('./image_7548.pdf')
 plt.show()
-
