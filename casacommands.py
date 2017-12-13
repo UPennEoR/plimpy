471c96d5729389224172fb7a6445cb05bbf6123e
TashaleeB <tashalee@sas.upenn.edu>
2017-12-13 11:09:47 -0500

    Add files via upload

--- casacommands.py
+++ casacommands.py
@@ -7,11 +7,10 @@
     """
 
 import numpy as np
-from astropy.io import fits #for python only
 import matplotlib.pyplot as plt
-import glob
-import os
+import glob,os
 
+from astropy.io import fits #for python only
 from fitsconverter import * #for python only
 
 #Convert from .uvfits to .uvfitsMS
@@ -23,21 +22,21 @@ for uvfit in uvfits:
 #*******************************************************************
 
 
-def reorder(msname):
+def reorrder(msname):
     import casac
     ms=casac.casac.table()
     ms.open(msname,nomodify=False)
     a1 , a2 , data = [ms.getcol(x) for x in [ "ANTENNA1" , "ANTENNA2" , "DATA" ]]
     m = a1 > a2
-    data[:, :, m]= data[:, :, m].conj()
-    x = a2[m]
-    a2[m] = a1[m]
-    a1[m] = x
-    ms.putcol("ANTENNA1", a1)
-    ms.putcol("ANTENNA2", a2)
-    ms.putcol("DATA", data)
-    ms.flush()
-    ms.close()
+    data [: ,: ,m]= data [: ,: , m ]. conj ()
+    x = a2 [ m ]
+    a2 [ m ]= a1 [ m ]
+    a1 [ m ]= x
+    ms.putcol("ANTENNA1",a1)
+    ms.putcol("ANTENNA2",a2)
+    ms.putcol("DATA",data)
+    ms.flush ()
+    ms.close ()
 
 def flag(msname): #You might have to update this
     flagdata(msname, flagbackup=True, mode='manual',antenna="22" )
@@ -46,24 +45,24 @@ def flag(msname): #You might have to update this
     flagdata(msname, flagbackup=True, mode='manual',antenna="81" )
 
 def gen_image(msname,imagename): # Makes image
-    clean(msname,imagename=imagename, niter=500, weighting='briggs', robust=0, imsize=[512,512], cell=['500 arcsec'], mode='mfs', nterms=1, spw='0:150~900', stokes='IQUV')
+    clean(msname,imagename=imagename,niter =500,weighting = 'briggs',robust =0,imsize =[512 ,512] ,cell=['500 arcsec'] ,mode='mfs',nterms =1,spw='0:150~900',stokes='IQUV')
 
 def mkinitmodel(msname,ext): #Model you give casa
-    cl.addcomponent(flux=1.0, fluxunit='Jy', shape='point', dir='J2000 17h45m40.0409s -29d0m28.118s')
+    cl.addcomponent(flux =1.0 ,fluxunit='Jy', shape = 'point' ,dir='J2000 17h45m40.0409s -29d0m28.118s')
     cl.rename('GC'+ext+'.cl')
     cl.close()
-    ft(msname, complist='GC'+ext+'.cl', usescratch=True)
+    ft(msname , complist = 'GC'+ext+'.cl' , usescratch = True )
 
 def clear_cal(msname):
     clearcal(msname)
 
 def phscal(msname):
     import os,sys
-    kc = os.path.basename(msname) + "K.cal"
-    bc = os.path.basename(msname) + "B.cal"
-    gaincal(msname,caltable=kc, gaintype='K', solint='inf', refant='10')
+    kc = os.path.basename(msname)  + "K.cal"
+    bc = os.path.basename(msname)  + "B.cal"
+    gaincal(msname,caltable=kc,gaintype = 'K' , solint='inf',refant='10')
     applycal(msname,gaintable=[kc])
-    bandpass(msname,caltable=bc, solint='inf', combine='scan', refant='10')
+    bandpass(msname,caltable=bc,solint='inf',combine='scan',refant='10')
     applycal(msname,gaintable=[bc])
 
 
@@ -79,7 +78,7 @@ for ms in MSfilelist:
     nwms.append(msf+'.R')
 
 i=0
-for i in np.arange(len(MSfilelist)):
+for i in np.arange(len(MSfilelist)): #This is the longest part
     reorrder(msname=name[i])# Reorder antenna 1 & 2 in each correlation so 2 is greater than 1
     flag(msname=name[i])# Flagging bad frequency channels or antennas and autoccorelations in CASA
     gen_image(msname=name[i],imagename=imgnam1+nwms[i])# Imaging
@@ -92,9 +91,8 @@ for i in np.arange(len(MSfilelist)):
 #%% CREATE .NPZ FILE IN CASA
 MSBCALlist=glob.glob('*MSB.cal')
 
-i=0
-for i in np.arange(len(MSBCALlist)):
-    tb.open(MSBCALlist[i])
+for msblist in MSBCALlist:
+    tb.open(msblist)
     gain = tb.getcol('CPARAM') # use 'FPARAM' when using K.cal files
     np.savez(nwms[i]+'.npz',gains=gain)
 
@@ -115,15 +113,10 @@ for im in imagename:
     exportfits(imagename,fitsimagename+ext)
 fitsimage = glob.glob(clean2*.fits)
 
+
 # This section is to be done in python
 test=fits.open('new_name.fits')
 test.info()
 test[0].header
 
-gen_image(msname=name[0],imagename=imgnam2+nwms[0])
-gen_image(name[1],imgnam1+nwms[1])
-gen_image(msname=name[1],imagename=imgnam2+nwms[1])
-gen_image(name[2],imgnam1+nwms[2])
-gen_image(msname=name[2],imagename=imgnam2+nwms[2])
-gen_image(name[3],imgnam1+nwms[3])
-gen_image(msname=name[3],imagename=imgnam2+nwms[3])
+
--- fitsconverter.py
+++ fitsconverter.py
@@ -7,28 +7,43 @@
 #printf "\e[?2004l"
 import numpy as np
 from pyuvdata import UVData
-import glob
-import os
+import glob, os
 
+#-------------------------------------------------------------------------------------------------------
+#IF YOU HAVE ONE POLARIZATION USE THIS
+#-------------------------------------------------------------------------------------------------------
 pollist = glob.glob('zen.2457548.46619.*.HH.uvcR')
-for i in np.arange(len(pollist)):
-    os.system('python add_uvws.py '+ pollist[i]+' -C hsa7458_v001')
-    os.system('python miriad2uvfits.py ' + pollist[i]+'U')
-#Now make a single miriad file
-files = glob.glob('*U')
+for pol in pollist:
+    os.system('python add_uvws.py '+ pol+' -C hsa7458_v001')
+    os.system('python miriad2uvfits.py ' + pol+'U')
+#-------------------------------------------------------------------------------------------------------
+#IF YOU HAVE ONE GROUP OF POLARIZATIONS (4 polarizations) USE THIS
+#-------------------------------------------------------------------------------------------------------
+pollist = glob.glob('zen.2457548.46619.*.HH.uvcR')
+for pol in pollist:
+    os.system('python add_uvws.py '+ pol+' -C hsa7458_v001')
 
+files = ['zen.2457548.46619.xx.HH.uvcR','zen.2457548.46619.yy.HH.uvcR','zen.2457548.46619.xy.HH.uvcR','zen.2457548.46619.yx.HH.uvcR'] #['zen.2457548.*.{xx,yy,xy,yx}.HH.uvcRU'] you can't use glob.glob because it will mess up the order of the polarizations
 uv = UVData()
 uv.read_miriad(files)
 uv.write_miriad('zen.2457548.46619.HH.uvcU')
-#OR IF YOU HAVE MORE THAN ONE GROUP OF POLARIZATIONS USE THIS
+#-------------------------------------------------------------------------------------------------------
+#IF YOU HAVE MORE THAN ONE GROUP OF POLARIZATIONS USE THIS
+#-------------------------------------------------------------------------------------------------------
+
+pollist = glob.glob('zen.2457548.46619.*.HH.uvcR')
+for pol in pollist:
+    os.system('python add_uvws.py '+ pol+' -C hsa7458_v001')
+
 #You have to make list and lst.
-#list1= glob.glob('zen.2457548.*.{xx,yy,xy,yx}.HH.uvcRU')
-# lst=[list1,list2,list3,....] these contain your list of grouped miriad files which will always have length 4 since there are only 4 polarizations.
-i=0
-for i in np.arange(len(lst)):
-    mf = lst[i][0].strip('xx.HH.uvcRU')
-    files=lst[i]
+#list1= ['zen.2457548.*.{xx,yy,xy,yx}.HH.uvcRU'] you can't use glob.glob because it will mess up the order of the polarizations
+# lists=[list1,list2,list3,....] these contain your list of grouped miriad files which will always have length 4 since there are only 4 polarizations.
+
+for lst in lists:
+    mf = lst[0].strip('xx.HH.uvcRU')
+    files=lst
     uv = UVData()
     uv.read_miriad(files)
     uv.write_miriad(mf+'.HH.uvcRU')
     os.system('python miriad2uvfits.py ' + mf+'.HH.uvcRU')
+
