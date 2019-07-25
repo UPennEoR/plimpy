4ef385143eede6b3fe06dbf51b341de6898e499f
Adam Weidman <adambis@sas.upenn.edu>
2019-07-25 11:10:19 -0600

    working

--- PolarizedMosaics/slicer_script.py
+++ PolarizedMosaics/slicer_script.py
@@ -17,7 +17,9 @@ import operator
 import astropy
 
 path = sys.argv[0]
-JD = sys.argv[1]
+pathToCalibrated = sys.argv[1]
+J = sys.argv[2]
+JD = str(J)
 
 '''def contain_final_flags(path):
     flagged = False
@@ -29,34 +31,34 @@ JD = sys.argv[1]
     return flagged'''
 
 def imaged_files (path):
-    imagefits = glob.glob(path + 'zen.*.HH.calibrated.uvh5_image/*.image.image.fits')
+    imagefits = glob.glob(str(path) + 'zen.*.HH.calibrated.uvh5_image/*.image.image.fits')
     uvh5files = []
     for imf in imagefits:
         tmp = os.path.split(imf)[-1].split('.')
-        uvh5files.append(path + tmp[0]+'.'+ tmp[1]+'.'+tmp[2]+'.HH.uvh5')
+        uvh5files.append(str(path) + tmp[0]+'.'+ tmp[1]+'.'+tmp[2]+'.HH.uvh5')
     return uvh5files
 
-def calibration(path):
+def calibration(pathToData, pathToCalibrated):
     cmd = "apply_cal.py --new_cal={} --filetype_in uvh5 --filetype_out uvh5 {} {}"
     
+    pathToData = str(path)
     JDs = []
-    im_files = imaged_files(path)
+    im_files = imaged_files(str(path))
     for f in im_files:
         JD = os.path.split(f)[0].split('zen.')[1].split('.HH')[0]
         JDs.append(JD)
 
     for f in JDs:
-        uvh5 = "zen." + str(f) + ".HH.uvh5"
-        cal = "zen." + str(f) + ".HH.calibrated.uvh5"
-        refca = "zen." + str(f) + ".HH.reflections.calfits"
-        flags = "zen." + str(f) + ".HH.final_flags.h5"
+        uvh5 = str(pathToData) + "zen." + str(f) + ".HH.uvh5"
+        cal =  pathToCalibrated + "zen." + str(f) + ".HH.calibrated.uvh5"
+        refca = str(pathToData) + "zen." + str(f) + ".HH.smooth_abs.calfits'"
     
         os.system(cmd.format(refca, uvh5, cal))
 
-def make_dictionary (JD, path, cont_flag):
+def make_dictionary (JD, path):
     
     #load in all files for that day
-    filenames = sorted(glob.glob(os.path.join(PATH, JD + '/zen.' + JD +'*.HH.calibrated.uvh5')))
+    filenames = sorted(glob.glob(os.path.join(pathToCalibrated, JD + '/zen.' + JD +'*.HH.calibrated.uvh5')))
     
     #create dictionary
     master_dict = {}
@@ -72,11 +74,11 @@ def make_dictionary (JD, path, cont_flag):
         
     return master_dict
 
-def find_files(JD, LSTstart, LSTend, cont_flag, master_Dict=None):
+def find_files(JD, LSTstart, LSTend, master_Dict=None):
     files = []
     
     if master_Dict is None:
-        master_Dict = make_dictionary (JD, path, cont_flag)
+        master_Dict = make_dictionary (JD, str(path))
     else:
         if type(master_Dict) is not dict:
             raise TypeError
@@ -96,8 +98,35 @@ def checkImageFiles(foundFiles, imagedFiles):
                 check = True
     return check
 
-def splitDay (JD, interval, cont_flag):
-    pathname = path + JD + '/'
+def max_min_LSTs(dict_list):
+    Max = 0
+    Min = 1e99
+    for i, (J, LST) in enumerate(dict_list):
+        if (Max < dict_list[i][1]): 
+            Max = dict_list[i][1]
+        if (Min > dict_list[i][1]): 
+            Min = dict_list[i][1]
+    return Max, Min
+
+def slice_day(JD, interval):
+    master_Dict = make_dictionary (JD, str(path))
+    dictionary_list = list(master_Dict)
+    
+    #max, min LST values
+    Max, Min = max_min_LSTs(dictionary_list)
+    
+    sliced = {}
+    
+    #this for-loop isn't correct it doesn't include all the ranges. what about last value?
+    for i in np.arange(Min, Max, interval):
+        sliced.update({(JD, i, i+interval): find_files(JD, i, i+interval, master_Dict)})
+        
+    sliced.update({(JD, i, i+interval): find_files(JD, i, Max, master_Dict)})
+    
+    return sliced, master_Dict
+
+def splitDay (JD, interval):
+    pathname = str(path) + JD + '/'
     sliced, master_dict = slice_day (JD, interval)
     imagefiles = imaged_files(pathname)
     
@@ -105,7 +134,7 @@ def splitDay (JD, interval, cont_flag):
     
     radDay = 2*np.pi
     for i in np.arange(0, radDay, interval):
-        files = find_files(JD, i, i+interval, cont_flag, master_dict)
+        files = find_files(JD, i, i+interval, master_dict)
         fi_li = list(files)
         
         #check if files have imaged files
@@ -128,12 +157,11 @@ def splitDay (JD, interval, cont_flag):
                 uvd.write_uvfits('zen.' + JD + '.' + str(i) + '-' + (str(i+interval)) + '.HH.calibrated.uvfits', 
                                  force_phase=True, spoof_nonessential=True)
 
-
 #calls
 #flagbool = contain_final_flags(path)
 
 #constant(s)
 interval = 2./(24*60.)*2*np.pi
 
-calibration(path)
-sliceDay(JD, interval, flagbool)
+calibration(path, pathToCalibrated)
+splitDay(JD, interval)
