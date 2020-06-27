fc16601b0115708f9a7394666d584209de1cbca4
Alexander Seidel <apseidel@Alexanders-MacBook-Air.local>
2020-06-26 20:13:47 -0400

    readability and example

--- PolarizedMosaics/image_maker.py
+++ PolarizedMosaics/image_maker.py
@@ -4,47 +4,53 @@ from pyuvdata import UVData
 from glob import glob
 import os
 
-def within_bounds(di, dra, dde, ra, de, max_angle, min_brightness):
-    #getting angular distance on a sphere from ra and dec isn't just the standard distance formula
-    cosa = np.sin(dde)*np.sin(de) + np.cos(dde)*np.cos(de)*np.cos(dra - ra)
+def within_bounds(di, min_brightness, dra, ddec, ra, dec, max_angle):
+    # a = angular distance on a sphere from ra and dec
+    cosa = np.sin(ddec)*np.sin(dec) + np.cos(ddec)*np.cos(dec)*np.cos(dra - ra)
     return np.logical_and(cosa > np.cos(max_angle), di > min_brightness)
+
+path = ''
+impath = ''
 
 #path = '/lustre/aoc/projects/hera/jaguirre/PolarizedMosaics/2458098/'
 #impath = '/lustre/aoc/projects/hera/aseidel/test_script/'
 
-path = '/home/alex/gleam clean boxes/script_test/'
-impath = '/home/alex/gleam clean boxes/script_test/images/'
-
-images = [x[len(path):-7] for x in glob(path+'zen.*.HH.calibrated.uvfits')]
+images = [x[len(path):-7] for x in glob(path+'*.uvfits')]
 images.sort()
 
-#check this
+# check this
 dec0 = -30.8
 
-for file in images:
-    imnamepath = impath+file
+data_hdul = fits.open('asu.fit')
+data = data_hdul[2].data
+
+for img in images:
+    imnamepath = impath + img
     uvd = UVData()
-    uvd.read_uvfits(path+file+".uvfits", read_data=False, read_metadata=True)
+    uvd.read_uvfits(path + img + ".uvfits", read_data=False, keep_all_metadata=True)
     ra0 = np.rad2deg(np.median(np.unique(uvd.lst_array)))
-    
-    data_hdul = fits.open('asu.fit')
-    data = data_hdul[2].data
-    in_bounds = data[within_bounds(data['Fintwide'], np.deg2rad(data['RAJ2000']), np.deg2rad(data['DEJ2000']), np.deg2rad(ra0), np.deg2rad(dec0), np.deg2rad(10.), 1)]
 
-    fornax = np.array([[(3+24/60.)/24.*360, -(37+16/60.), 260], [(3+(21+40/60.)/60.)/24.*360, -(37+10/60.), 490], [(3+(22+43/60.)/60.)/24.*360, -(37+(12+2/60.)/60.), 2]])
-    fornax_inb = fornax[within_bounds(fornax[:,2], np.deg2rad(fornax[:,0]), np.deg2rad(fornax[:,1]), np.deg2rad(ra0), np.deg2rad(dec0), np.deg2rad(30.), 0)]
+    all_angles = [np.deg2rad(x) for x in [data['RAJ2000'], data['DEJ2000'], ra0, dec0, 10]]
+    in_bounds = data[within_bounds(data['Fintwide'], 1, *all_angles)]
 
+    # additional sources not in GLEAM [ra, dec, intensity]
+    fornax = np.array([[(3 + 24         /60)/24 * 360, -(37 + 16/60), 260],
+                       [(3 +(21 + 40/60)/60)/24 * 360, -(37 + 10/60), 490],
+                       [(3 +(22 + 43/60)/60)/24 * 360, -(37 +(12+2/60)/60), 2]])
+    f_angles = [np.deg2rad(x) for x in [fornax[:,0], fornax[:,1], ra0, dec0, 30]]
+    f_inb = fornax[within_bounds(fornax[:,2], 1, *f_angles)]
+
+    make_circles = lambda d : ["circle[[{0}deg, {1}deg], 0.5deg]\n".format(
+                                        str(x[0]), str(x[1])) for x in d]
     mask_file = open(imnamepath+".masks.txt", "w")
     mask_file.write("#CRTFv0\n")
-    mask_file.writelines(["circle[[" + str(x[0]) + "deg, " + str(x[1]) + "deg], 0.5deg]\n" for x in in_bounds])
-    mask_file.writelines(["circle[[" + str(x[0]) + "deg, " + str(x[1]) + "deg], 0.5deg]\n" for x in fornax_inb])
+    mask_file.writelines(make_circles(in_bounds) + make_circles(f_inb))
     mask_file.close()
 
-    summary = open(imnamepath+".info.txt", "w")
-    summary.write(path+file[:32]+".uvfits\n")
+    summary = open(imnamepath + ".info.txt", "w")
+    summary.write(path + img[:32] + ".uvfits\n")
     summary.write(str(ra0) + "\n")
     summary.write(str(dec0) + "\n")
     summary.close()
 
-os.system("casa -c image_maker_casa.py \"%s\"" % impath)
-
+os.system("/Applications/CASA.app/Contents/MacOS/casa -c image_maker_casa.py \"%s\"" % impath)
--- PolarizedMosaics/image_maker_casa.py
+++ PolarizedMosaics/image_maker_casa.py
@@ -8,10 +8,19 @@ for file in images:
     summary = open(file+".info.txt", "r")
     fitsfile, ra0, dec0 = [x[:-1] for x in summary.readlines()]
     importuvfits(fitsfile, file+".ms")
-    
-    tclean(vis=file+'.ms', imagename=file+'.no_deconvolution', niter=0, weighting='briggs', robust=0, imsize = [512,512], pbcor=False, cell=['500 arcsec'], specmode='mfs', nterms=1, spw='0:100~920', stokes='IQUV', interactive=False, pblimit=-1, phasecenter='J2000 %sdeg %sdeg' % (ra0, dec0))
-    
-    tclean(vis=file+'.ms', imagename=file+'.deconvolved', niter=5000, weighting='briggs', robust=0, imsize = [512,512], pbcor=False, cell=['500 arcsec'], specmode='mfs', nterms=1, spw='0:100~920', stokes='IQUV', mask=file+'.masks.txt', interactive=False, cycleniter=1000, threshold='1Jy/beam', pblimit=-1, phasecenter='J2000 %sdeg %sdeg' % (ra0, dec0))
+
+    tclean(vis=file+'.ms', imagename=file+'.no_deconvolution', niter=0,
+            weighting='briggs', robust=0, imsize = [512,512], pbcor=False,
+            cell=['500 arcsec'], specmode='mfs', nterms=1, spw='0:100~920',
+            stokes='IQUV', interactive=False, pblimit=-1,
+            phasecenter='J2000 %sdeg %sdeg' % (ra0, dec0))
+
+    tclean(vis=file+'.ms', imagename=file+'.deconvolved', niter=5000,
+            weighting='briggs', robust=0, imsize = [512,512], pbcor=False,
+            cell=['500 arcsec'], specmode='mfs', nterms=1, spw='0:100~920', 
+            stokes='IQUV', mask=file+'.masks.txt', interactive=False,
+            cycleniter=1000, threshold='1Jy/beam', pblimit=-1,
+            phasecenter='J2000 %sdeg %sdeg' % (ra0, dec0))
 
     exportfits(file+".no_deconvolution.image", file+".no_deconvolution.fits")
     exportfits(file+".deconvolved.image", file+".deconvolved.fits")
