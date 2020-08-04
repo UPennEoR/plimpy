3b9fde81037b42d77edb38b5c149d973b07fcbe2
Alexander Seidel <aseidel@herapost-master.aoc.nrao.edu>
2020-08-04 12:32:37 -0600

    changes to plot_all

--- PolarizedImaging/mask_and_none.py
+++ PolarizedImaging/mask_and_none.py
@@ -1,51 +1,123 @@
 import numpy as np
 from astropy.io import fits
+from astropy import wcs
 from pyuvdata import UVData
 from glob import glob
 import os
 
-def within_bounds(di, min_brightness, dra, ddec, ra, dec, max_angle):
-    # a = angular distance on a sphere from ra and dec
-    cosa = np.sin(ddec)*np.sin(dec) + np.cos(ddec)*np.cos(dec)*np.cos(dra - ra)
-    return np.logical_and(cosa > np.cos(max_angle), di > min_brightness)
 
-mspath = '/lustre/aoc/projects/hera/plaplant/HERA19Golden/CalibratedData/2457548/zen.2457548.26437.HH.uvcRPCS.MS'
-impath = '/lustre/aoc/projects/hera/aseidel/mask_and_none_flags2/'
+uvh5path = '/lustre/aoc/projects/hera/jaguirre/HERA19Summer2020/Simulation/zen.2457755.74025.uvCP.uvh5'
+impath = '/lustre/aoc/projects/hera/aseidel/mask_and_none_simulated/'
 
 os.system("rm -rf {}*".format(impath))
-os.system("casa -c \"tclean(vis='{}',imagename='{}no_deconvolution',".format(mspath, impath) + 
+
+uvd = UVData()
+uvd.read_uvh5(uvh5path)
+
+name = '.'.join(uvh5path.split('/')[-1].split('.')[:-1])
+uvfitspath = impath + name + '.uvfits'
+mspath = impath + name + '.ms'
+
+uvd.write_uvfits(uvfitspath, spoof_nonessential=True, force_phase=True)
+
+#mspath = '/lustre/aoc/projects/hera/plaplant/HERA19Golden/CalibratedData/2457548/zen.2457548.26437.HH.uvcRPCS.MS'
+#mspath = '/lustre/aoc/projects/hera/nkern/CHAMP_Bootcamp/Lesson10_HERADataPartII/data/zen.2458116.24482.xx.HH.uvOCR.ms'
+
+
+#generate image without deconvolution
+os.system("casa -c \"importuvfits(fitsfile='{}',vis='{}') ; ".format(uvfitspath, mspath) +
+                "tclean(vis='{}',imagename='{}no_deconvolution',".format(mspath, impath) + 
                 "niter=0, imsize = [512,512], cell=['500 arcsec']," +
                 "specmode='mfs', spw='0:100~920', stokes='IQUV'," + 
                 "interactive=False, pblimit=-1, gridder='widefield') ; " +
                 "exportfits(imagename='{}no_deconvolution.image',fitsimage='{}')\"".format(
-                    impath, impath+'data.fits'))
+                    impath, impath+'no_deconvolution.fits'))
 
-header = fits.open(impath+'data.fits')[0].header
+#make wcs solution
+header = fits.open(impath+'no_deconvolution.fits')[0].header
 ra0, dec0 = header['CRVAL1'], header['CRVAL2']
+w = wcs.WCS(header, naxis=2)
+
+#open the primary beam fits file
+primary_beam = fits.open("muellerbeam.fits")[0].data[0,0,0,:,:]
 
 data_hdul = fits.open('/lustre/aoc/projects/hera/aseidel/summer_2019/asu.fit')
-data = data_hdul[2].data
+gleam_data = data_hdul[2].data
 
-for minb in [0.5, 1.0]:
-    all_angles = [np.deg2rad(x)
-                    for x in [data['RAJ2000'], data['DEJ2000'], ra0, dec0, 10]]
-    in_bounds = data[within_bounds(data['Fintwide'], minb, *all_angles)]
 
-    # additional sources not in GLEAM [ra, dec, intensity]
-    fornax = np.array([[(3 + 24         /60)/24 * 360, -(37 + 16/60), 260],
-                       [(3 +(21 + 40/60)/60)/24 * 360, -(37 + 10/60), 490],
-                       [(3 +(22 + 43/60)/60)/24 * 360, -(37 +(12+2/60)/60), 2]])
-    f_angles = [np.deg2rad(x)
-                    for x in [fornax[:,0], fornax[:,1], ra0, dec0, 30]]
-    f_inb = fornax[within_bounds(fornax[:,2], 1, *f_angles)]
+def effective_intensity(ra, dec, intensity):
+    
+    # get primary beam at each source
+    pbx, pby = np.array(w.all_world2pix(ra, dec, 0)).astype(int)
+    pb_inb = np.logical_and.reduce(
+        (pbx >= 0, pby >= 0, pbx < 512, pby < 512))
+    pb = primary_beam[pbx * pb_inb, pby * pb_inb] * pb_inb
+    return intensity * pb 
 
-    make_circles = lambda d : ["circle[[{0}deg, {1}deg], 0.5deg]\n".format(
+
+def within_bounds(ra, dec, intensity, min_brightness):
+    return effective_intensity(ra, dec, intensity) > min_brightness
+
+#def within_bounds(di, min_brightness, dra, ddec, ra, dec, max_angle):
+#    # a = angular distance on a sphere from ra and dec
+#    cosa = np.sin(ddec)*np.sin(dec) + np.cos(ddec)*np.cos(dec)*np.cos(dra - ra)
+#    return np.logical_and(cosa > np.cos(max_angle), di > min_brightness)
+
+
+
+make_circles = lambda d : ["circle[[{0}deg, {1}deg], 0.5deg]\n".format(
                                         str(x[0]), str(x[1])) for x in d]
-    mask_file = open(impath+"{:.2f}Jymask.txt".format(minb), "w")
-    mask_file.write("#CRTFv0\n")
-    mask_file.writelines(make_circles(in_bounds) + make_circles(f_inb))
-    mask_file.close()
 
+minbs = [0.25, 0.5] #min effective brightness (Jy)
+
+#for minb in minbs:
+#    in_bounds = gleam_data[within_bounds(gleam_data['RAJ2000'], gleam_data['DEJ2000'], gleam_data['Fintwide'], minb)]
+#
+#    # additional sources not in GLEAM [intensity, ra, dec]
+#    fornax = np.array([[(3 + 24         /60)/24 * 360, -(37 + 16/60), 260],
+#                       [(3 +(21 + 40/60)/60)/24 * 360, -(37 + 10/60), 490],
+#                       [(3 +(22 + 43/60)/60)/24 * 360, -(37 +(12+2/60)/60), 2]])
+#    f_inb = fornax[within_bounds(*fornax.T, minb)]
+#
+#    name = impath + "new_{:.2f}Jymask.txt".format(minb)
+#    os.system('rm -f {}'.format(name))
+#    mask_file = open(name, "w")
+#    mask_file.write("#CRTFv0\n")
+#    mask_file.writelines(make_circles(in_bounds) + make_circles(f_inb))
+#    mask_file.close()
+    
+    
+#for minb in [0.5, 1.0]:
+#    all_angles = [np.deg2rad(x)
+#                    for x in [data['RAJ2000'], data['DEJ2000'], ra0, dec0, 10]]
+#    in_bounds = data[within_bounds(data['Fintwide'], minb, *all_angles)]
+#
+#    # additional sources not in GLEAM [ra, dec, intensity]
+#    fornax = np.array([[(3 + 24         /60)/24 * 360, -(37 + 16/60), 260],
+#                       [(3 +(21 + 40/60)/60)/24 * 360, -(37 + 10/60), 490],
+#                       [(3 +(22 + 43/60)/60)/24 * 360, -(37 +(12+2/60)/60), 2]])
+#    f_angles = [np.deg2rad(x)
+#                    for x in [fornax[:,0], fornax[:,1], ra0, dec0, 30]]
+#    f_inb = fornax[within_bounds(fornax[:,2], 1, *f_angles)]
+#
+#    
+#    mask_file = open(impath+"{:.2f}Jymask.txt".format(minb), "w")
+#    mask_file.write("#CRTFv0\n")
+#    mask_file.writelines(make_circles(in_bounds) + make_circles(f_inb))
+#    mask_file.close()
+
+mask_file = no_mask = open(impath+"10deg_mask.txt", "w")
+mask_file.write("#CRTFv0\n")
+mask_file.write("circle[[{0}deg, {1}deg], 10deg]\n".format(
+                                        str(ra0), str(dec0)))
+mask_file.close()
+
+no_mask = open(impath+"no_msk.txt", "w")
+no_mask.write("#CRTFv0\n")
+no_mask.write("circle[[{0}deg, {1}deg], 50deg]\n".format(
+                                        str(ra0), str(dec0)))
+no_mask.close()
+    
 summary = open(impath + "info.txt", "w")
 summary.write(str(ra0) + "\n")
 summary.write(str(dec0) + "\n")
--- PolarizedImaging/mask_and_none_casa.py
+++ PolarizedImaging/mask_and_none_casa.py
@@ -6,7 +6,7 @@ mspath = sys.argv[3]
 impath = sys.argv[4]
 
 total_niter = [1000, 3000, 5000]
-mask_proportion = [0.0, 1.0]
+mask_proportion = [0.0, 0.5, 0.8, 1.0]
 
 summary = open(impath+"info.txt", "r")
 ra0, dec0 = [x[:-1] for x in summary.readlines()]    
@@ -14,7 +14,6 @@ ra0, dec0 = [x[:-1] for x in summary.readlines()]
 os.system("cp -r {} {}".format(mspath, impath))
 
 mspath = impath + mspath.split('/')[-1]
-flagdata(vis=mspath, antenna="23,44,81,82")
 
 #tclean(vis=mspath,imagename=impath+'no_deconvolution',
 #                niter=1, imsize = [512,512], cell=['500 arcsec'],
@@ -30,8 +29,11 @@ for n in total_niter:
         n2 = n - n1
         
         for mpath in masks:
-            m = mpath[-14:-8]
-            newimage = impath+'n->{}_p->{:.2f}_mask->{}'.format(n,p,m)
+            mname = mpath.split('/')[-1].split('_')[-1]
+            m = '.'.join(mname.split('.')[:-1])
+            newimage = impath+'n{}_p{:.2f}_mask{}'.format(n,p,m)
+            print(newimage.split('/')[-1])
+            
             tclean(vis=mspath, imagename=newimage,
                     niter=n1, weighting='briggs', robust=0.5, imsize = [512,512], cell=['500 arcsec'],
                     specmode='mfs', nterms=2, mask=mpath, spw='0:100~920',stokes='IQUV', 
@@ -46,7 +48,7 @@ for n in total_niter:
             
             tclean(vis=mspath, imagename=newimage,
                     niter=n2, weighting='briggs', robust=0.5, imsize = [512,512], cell=['500 arcsec'],
-                    specmode='mfs', nterms=2, spw='0:100~920',stokes='IQUV', 
+                    specmode='mfs', nterms=2, mask=impath+'no_msk.txt', spw='0:100~920',stokes='IQUV', 
                     interactive=False, pblimit=-1, #phasecenter='J2000 {}deg {}deg'.format(ra0, dec0),
                     gridder='widefield')
             
