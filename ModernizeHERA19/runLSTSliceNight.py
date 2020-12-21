de6024d3865c78b497cfe0a4d2670e943e923090
James Aguirre <jaguirre@sas.upenn.edu>
2020-12-21 16:32:13 -0500

    Mon Dec 21 16:32:13 EST 2020

---
+++ ModernizeHERA19/runLSTSliceNight.py
@@ -0,0 +1,28 @@
+#!/Users/jaguirre/opt/anaconda3/envs/hera/bin/python
+
+#/usr/bin/python
+
+import sys, getopt, os
+
+def main(argv):
+   JD = ''
+   try:
+      opts, args = getopt.getopt(argv,"hj:",["jd="])
+   except getopt.GetoptError:
+      print('test.py -j <julian_date>')
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print('runLSTSliceNight.py -j <julian_date>')
+         sys.exit()
+      elif opt in ("-j", "--jd"):
+         JD = arg
+   print('Julian date is %s' % JD)
+
+   if JD != '':
+
+       import LSTSliceNight
+       LSTSliceNight.sliceJD(JD)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
