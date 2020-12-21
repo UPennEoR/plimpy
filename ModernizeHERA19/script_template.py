724108ed37dd6b508677803fb6641c23a533d681
James Aguirre <jaguirre@sas.upenn.edu>
2020-12-21 15:53:42 -0500

    Mon Dec 21 15:53:42 EST 2020

---
+++ ModernizeHERA19/script_template.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+import sys, getopt, os
+
+def main(argv):
+   inputfile = ''
+   outputfile = ''
+   try:
+      opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+   except getopt.GetoptError:
+      print 'test.py -i <inputfile> -o <outputfile>'
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print 'test.py -i <inputfile> -o <outputfile>'
+         sys.exit()
+      elif opt in ("-i", "--ifile"):
+         inputfile = arg
+      elif opt in ("-o", "--ofile"):
+         outputfile = arg
+   print 'Input file is "', inputfile
+   print 'Output file is "', outputfile
+
+   print(os.system('which python'))
+
+   import healpy
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
