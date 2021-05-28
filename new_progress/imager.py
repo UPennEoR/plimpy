cbcaf984ccc98d951fcc8592dc47659e6c060f82
JianrongTan <jianrongtan096@gmail.com>
2021-05-28 11:45:00 -0600

    Summary May28

--- new_progress/imager.py
+++ new_progress/imager.py
@@ -13,15 +13,54 @@ import cv2
 
 ## A wrapper for converting uvh5(uvfits) files to fits files and 
 ## producing images (casa) and videos 
-def uvh5_2_fits(filename, outputpath, 
-                good_ants=list(set([9,10,20,22,31,43,53,64,65,72,80,81,88,89,96,97,104,105,112]) - set([22,43,80,81])), ra=0, dec=0, niter=0, \
+def overlay_dirty_images(fitsfile_1, fitsfile_2, title=None, save_fig=False):
+    stokes=["I",'Q','U','V']
+    hdu_1 = fits.open(fitsfile_1)
+    image_wcs = WCS(hdu_1[0].header, naxis=[1,2])
+    image_1 = hdu_1[0].data
+    
+    hdu_2 = fits.open(fitsfile_2)
+    image_2 = hdu_2[0].data
+    
+    fig, axes = plt.subplots(figsize=(15,10), dpi=300, nrows=2, ncols=2, sharex=True, sharey=True, 
+                             subplot_kw = {'projection' : image_wcs})
+    fig.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=None, hspace=0.2)
+    for i in range(4):
+        ax = axes[i//2,i%2]
+        vmin, vmax = np.min(image_1[i,0,:,:]), np.max(image_1[i,0,:,:])
+        im_1 = ax.imshow(image_1[i,0,:,:], origin='lower', interpolation='nearest', cmap='Reds', aspect='auto', vmin=vmin, vmax=vmax)
+        im_2 = ax.imshow(image_2[i,0,:,:], origin='lower', interpolation='nearest', cmap='Blues', alpha=0.8, aspect='auto', vmin=vmin, vmax=vmax)            
+        ax.grid(color='white',alpha=0.5)
+        cbar_2 = fig.colorbar(im_2, ax=ax, pad=0.08)
+        cbar_2.ax.tick_params(labelsize=10)
+        cbar_1 = fig.colorbar(im_1, ax=ax, pad=0.08)
+        cbar_1.ax.tick_params(labelsize=10)
+
+        ax.text(0.1,0.9, "{}".format(stokes[i]), bbox=dict(facecolor='white', alpha=0.1), transform=ax.transAxes, fontsize=20, color='k')
+        ax.tick_params(labelsize=16)
+        # There's unquestionably a better way to get matplotlib to share axes
+        if (i == 2 or i == 3):
+            ax.set_xlabel('RA (J2000)', fontsize=16)
+        else:
+            ax.set_xlabel(' ')
+        if (i == 0 or i == 2):
+            ax.set_ylabel('Dec (J2000)', fontsize=16)
+        else:
+            ax.set_ylabel(' ')
+        ax.tick_params(axis='both', which='both', labelsize=16)
+    if title is not None:
+        fig.suptitle(str(title), fontsize=16, y=0.95)
+    if save_fig:
+        fig.savefig(fitsfile[:-5]+".png", dpi=300, bbox_inches='tight')
+    plt.show()
+    
+def uvh5_2_fits(filename, outputpath, ra=0, dec=0, niter=0, \
            weighting='briggs', robust=0, imsize=[512,512], pbcor=False, cell=['500 arcsec'], \
            specmode='mfs', nterms=1, stokes='IQUV', interactive=False, pblimit=-1, **kwargs):
     uvd = UVData()
-    uvd.read(filename, antenna_nums=good_ants)
+    uvd.read(filename)
     if uvd.phase_type != 'phased':
-        uvd.phase_to_time(np.unique(uvd.time_array)[uvd.Ntimes//2])
-    dec, ra = uvd.phase_center_dec_degrees, uvd.phase_center_ra_degrees
+        uvd.phase(ra, dec)
     if filename.endswith(".uvh5"):
         uvfitsfile = outputpath + filename.split('/')[-1][:-5] + ".uvfits"
         uvd.write_uvfits(uvfitsfile,spoof_nonessential=True,force_phase=True)
@@ -38,20 +77,20 @@ def uvh5_2_fits(filename, outputpath,
     tclean(vis=msfile, imagename=imagefilestem, niter=niter, \
            weighting=weighting, robust=robust, imsize=imsize, pbcor=pbcor, cell=cell, \
            specmode=specmode, nterms=nterms, stokes=stokes, interactive=interactive, pblimit=pblimit, 
-           phasecenter='J2000 %sdeg %sdeg' % (ra, dec), **kwargs)
+           phasecenter='J2000 %srad %srad' % (ra, dec), **kwargs)
     exportfits(imagename=imagefilestem + ".image", fitsimage=msfile[:-3]+'.fits', overwrite=True)
     
-def fits_2_image(fitsfile):
+def fits_2_image(fitsfile, title=None, save_fig=False):
     stokes=["I",'Q','U','V']
     hdu = fits.open(fitsfile)
     image_wcs = WCS(hdu[0].header, naxis=[1,2])
     image = hdu[0].data
-    fig, axes = plt.subplots(figsize=(18,16), dpi=300, nrows=2, ncols=2, sharex=True, sharey=True, 
+    fig, axes = plt.subplots(figsize=(14,12), dpi=300, nrows=2, ncols=2, sharex=True, sharey=True, 
                              subplot_kw = {'projection' : image_wcs})
     fig.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=None, hspace=0.2)
     for i in range(4):
         ax = axes[i//2,i%2]
-        im = ax.imshow(image[i,0,:,:], origin='lower', aspect='auto')
+        im = ax.imshow(image[i,0,:,:], origin='lower', aspect='auto', interpolation='nearest',)
         ax.grid(color='white',alpha=0.75)
         cbar = fig.colorbar(im, ax=ax)
         cbar.ax.tick_params(labelsize=16)
@@ -67,7 +106,10 @@ def fits_2_image(fitsfile):
         else:
             ax.set_ylabel(' ')
         ax.tick_params(axis='both', which='both', labelsize=16)
-    fig.savefig(fitsfile[:-5]+".png", dpi=300, bbox_inches='tight')
+    if title is not None:
+        fig.suptitle(str(title), fontsize=16, y=0.95)
+    if save_fig:
+        fig.savefig(fitsfile[:-5]+".png", dpi=300, bbox_inches='tight')
     plt.show()
     
 def image_2_video(imagefiles, output_filename, output_format=".avi", fps=4.0):
